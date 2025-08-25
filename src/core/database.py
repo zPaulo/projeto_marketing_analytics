b87973e5fff45c0c1bd7509a642801b673235429
zPaulo <pauloarruda5@protonmail.com>
2025-08-25 17:54:30 -0300

    Adiciona módulo core com configurações de banco de dados e settings

---
+++ src/core/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from settings import settings
+
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
