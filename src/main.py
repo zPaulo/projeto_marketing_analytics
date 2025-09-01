7237c861e6b7660f9d0b5a1be30e65d54c763e48
zPaulo <pauloarruda5@protonmail.com>
2025-08-31 21:01:09 -0300

    feat: atualiza main.py com nova orquestração
    - Integra novos pipelines na aplicação principal
    - Melhora estrutura de execução dos pipelines
    - Adiciona suporte para execução múltipla de pipelines

--- src/main.py
+++ src/main.py
@@ -6,10 +6,16 @@ project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root)
 
 from src.pipelines.telco.pipeline import TelcoPipeline
+from src.pipelines.olist.pipeline import OlistPipeline
+
 
 async def main():
-    pipeline = TelcoPipeline()
-    await pipeline.run_elt()
+
+    pipeline_olist = OlistPipeline()
+    await pipeline_olist.run_elt()
+
+    pipeline_telco = TelcoPipeline()
+    await pipeline_telco.run_elt()
 
 # Para testes
 if __name__ == "__main__":
